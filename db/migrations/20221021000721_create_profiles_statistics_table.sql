69f559b9a163776caad4a584b077ea5cacf922f3
simonsan <14062932+simonsan@users.noreply.github.com>
2022-10-21 02:19:17 +0200

    Add profile_statistics migration with ideas

---
+++ db/migrations/20221021000721_create_profiles_statistics_table.sql
@@ -0,0 +1,32 @@
+-- migrate:up
+CREATE TABLE "profile_statistics" (
+	"ulid" TEXT(26) NOT NULL PRIMARY KEY,
+    -- "game_ulid_ref" TEXT(26), -- REL
+    -- "leaderboards_ulid_ref" TEXT(26), -- REL
+	"timestamp_dt" DATETIME NOT NULL,
+    "interval_days" INTEGER,
+    -- TODO: Talk to Coolio on Discord
+    -- IDEAS:
+    -- - how many games per week
+    -- - how many losses/wins
+    -- - more chatty?
+    -- - which games played most? how long?
+    -- - most played civs (favourite civs)
+    -- - most wins with which civs
+    -- - most played map
+    -- - most won map (which civ?)
+    -- - how much time spent in queue
+    -- - percentage of 1v1 vs TG (both ranked)
+    -- - tends to pick civ (metric?)
+    -- - etc.
+	-- CONSTRAINT "statistics_game_ulid_ref_fkey" FOREIGN KEY ("game_ulid_ref") REFERENCES "games" ("ulid"),
+	-- CONSTRAINT "statistics_leaderboards_ulid_ref_fkey" FOREIGN KEY ("leaderboards_ulid_ref") REFERENCES "leaderboards" ("ulid")
+);
+
+-- CREATE INDEX "statistics_timestamp_dt_IDX" ON "statistics" ("timestamp_dt");
+-- CREATE INDEX "statistics_game_ulid_ref_IDX" ON "statistics" ("game_ulid_ref");
+-- CREATE INDEX "statistics_leaderboards_ulid_ref_IDX" ON "statistics" ("leaderboards_ulid_ref");
+
+-- migrate:down
+drop table "profile_statistics";
+
