cf70ea24d4f5b025dcf2b7e320e92276e6991cde
simonsan <14062932+simonsan@users.noreply.github.com>
2022-10-30 06:57:21 +0100

    Make match_settings unique

--- db/schema.sql
+++ db/schema.sql
@@ -91,7 +91,32 @@ CREATE TABLE IF NOT EXISTS "tbl_match_settings" (
     "team_together" BOOLEAN,
     "treaty_length" INTEGER,
     "turbo_mode" BOOLEAN,
-    "victory_condition" SMALLINT
+    "victory_condition" SMALLINT,
+    UNIQUE (
+        "match_setting_ulid",
+        "allow_cheats",
+        "difficulty",
+        "empire_wars_mode",
+        "ending_age",
+        "full_tech_tree",
+        "game_mode",
+        "lock_speed",
+        "lock_teams",
+        "population",
+        "record_game",
+        "regicide_mode",
+        "resources",
+        "reveal_map",
+        "shared_exploration",
+        "speed",
+        "starting_age",
+        "sudden_death_mode",
+        "team_positions",
+        "team_together",
+        "treaty_length",
+        "turbo_mode",
+        "victory_condition"
+    )
 );
 CREATE TABLE IF NOT EXISTS "tbl_matches" (
     "match_ulid" TEXT(26) PRIMARY KEY NOT NULL,
