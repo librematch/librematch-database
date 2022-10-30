69ae459ba232aff4680675c12ee77fd864253868
simonsan <14062932+simonsan@users.noreply.github.com>
2022-10-30 04:57:30 +0100

    Create todo folder for coming migrations

--- db/schema.sql
+++ db/schema.sql
@@ -73,31 +73,6 @@ CREATE TABLE IF NOT EXISTS "tbl_profiles_relations" (
     FOREIGN KEY ("secondary_profile_ulid_ref") REFERENCES "tbl_profiles" ("profile_ulid")
 );
 CREATE UNIQUE INDEX "profiles_relations_IDX" ON "tbl_profiles_relations" ("main_profile_ulid_ref","secondary_profile_ulid_ref");
-CREATE TABLE IF NOT EXISTS "tbl_teams" (
-	"team_ulid" TEXT(26) NOT NULL PRIMARY KEY,
-	"name" TEXT(255) NOT NULL,
-	"is_archived" BOOLEAN DEFAULT FALSE NOT NULL,
-	"in_game_tag" TEXT(15) NULL,
-	"liquipedia_id" TEXT(50) NULL,
-	"discord_invite" TEXT(50) NULL,
-	"twitch_id" TEXT(50) NULL,
-	"twitter_id" TEXT(50) NULL,
-	"youtube_url" TEXT(50) NULL,
-	"fbgaming_id" TEXT(50) NULL
-);
-CREATE INDEX "teams_is_archived_IDX" ON "tbl_teams" ("is_archived");
-CREATE TABLE IF NOT EXISTS "tbl_teams_profiles_relations" (
-	"team_profile_relation_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"team_ulid_ref" TEXT(26) NOT NULL, -- A team
-	"profile_ulid_ref" TEXT(26) NOT NULL, -- can have many players
-	"game_ulid_ref" TEXT(26) NOT NULL, -- playing on different games
-    FOREIGN KEY ("team_ulid_ref") REFERENCES "tbl_teams" ("team_ulid"),
-    FOREIGN KEY ("profile_ulid_ref") REFERENCES "tbl_profiles" ("profile_ulid"),
-    FOREIGN KEY ("game_ulid_ref") REFERENCES "tbl_games" ("game_ulid")
-);
-CREATE TABLE sqlite_sequence(name,seq);
-CREATE UNIQUE INDEX "teams_profiles_relations_IDX" ON "tbl_teams_profiles_relations" ("team_ulid_ref", "profile_ulid_ref");
-CREATE INDEX "teams_games_relations_IDX" ON "tbl_teams_profiles_relations" ("game_ulid_ref");
 CREATE TABLE IF NOT EXISTS "tbl_match_settings" (
     "match_setting_ulid" TEXT(26) NOT NULL PRIMARY KEY,
     "allow_cheats" BOOLEAN,
@@ -176,6 +151,7 @@ CREATE TABLE IF NOT EXISTS "workflow_matches_import_pending" (
     "profile_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "priority" INTEGER NOT NULL
 );
+CREATE TABLE sqlite_sequence(name,seq);
 CREATE TABLE IF NOT EXISTS "workflow_matches_raw" (
     "match_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "json" TEXT NOT NULL,
@@ -261,92 +237,6 @@ CREATE TABLE IF NOT EXISTS "tbl_game_definitions" (
     CONSTRAINT "game_definitions_game_ulid_ref_fkey" FOREIGN KEY ("game_ulid_ref") REFERENCES "tbl_games" ("game_ulid")
 );
 CREATE UNIQUE INDEX "game_definitions_game_ulid_ref_IDX" ON "tbl_game_definitions" ("game_ulid_ref");
-CREATE TABLE IF NOT EXISTS "tbl_community_resources" (
-    "community_resource_ulid" TEXT(26) NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "has_https_enabled" BOOLEAN DEFAULT TRUE NOT NULL,
-    "description" TEXT(255),
-    "aoezone_id" TEXT(255) NULL,
-    "email_address" TEXT NULL,
-    "discord_id" TEXT NULL,
-    "discord_server_invite" TEXT NULL,
-    "contact_form" BOOLEAN DEFAULT FALSE NOT NULL,
-    "github_id" TEXT NULL,
-    "project_source_url" TEXT NULL
-);
-CREATE UNIQUE INDEX "community_resource_url_IDX" ON "tbl_community_resources" ("url");
-CREATE TABLE IF NOT EXISTS "tbl_tournaments" (
-    "tounament_ulid" TEXT(26) NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "short" TEXT(10),
-    "liquipedia_url" TEXT NULL,
-    "liquipedia_tier" SMALLINT NULL,
-    "start_dt" DATETIME NULL,
-    "end_dt" DATETIME NULL,
-    "prizepool" FLOAT NULL,
-    "player_amount" INTEGER NULL,
-    "location" TEXT NULL
-    -- TODO: Add more table columns
-);
-CREATE TABLE IF NOT EXISTS "tbl_statistics" (
-	"statistic_ulid" TEXT(26) NOT NULL PRIMARY KEY,
-    "game_ulid_ref" TEXT(26),
-    "leaderboard_ulid_ref" TEXT(26),
-	"timestamp_dt" DATETIME NOT NULL,
-    "interval_days" INTEGER,
-    "playerbase_size" INTEGER,
-    -- TODO: Check how the values of different intervals fit in here
-    -- TODO: Talk to Coolio on Discord
-    -- - interesting might be to make more obvious how many new multiplayers players are there (metric like first game on any leaderboard)
-    --   - we could even chose top5 (activity, rating) from them and make them queryable, to be e.g. greeted on community resources
-    --     - something like "We welcome @<username> as a new multiplayer to our community!"
-    --   - also how many people left the game (metric might be how many _new_ people haven't played the game in 1/1,5/2/3 months)
-	CONSTRAINT "statistics_game_ulid_ref_fkey" FOREIGN KEY ("game_ulid_ref") REFERENCES "tbl_games" ("game_ulid"),
-	CONSTRAINT "statistics_leaderboard_ulid_ref_fkey" FOREIGN KEY ("leaderboard_ulid_ref") REFERENCES "tbl_leaderboards" ("leaderboard_ulid")
-);
-CREATE INDEX "statistics_timestamp_dt_IDX" ON "tbl_statistics" ("timestamp_dt");
-CREATE INDEX "statistics_game_ulid_ref_IDX" ON "tbl_statistics" ("game_ulid_ref");
-CREATE INDEX "statistics_leaderboard_ulid_ref_IDX" ON "tbl_statistics" ("leaderboard_ulid_ref");
-CREATE TABLE IF NOT EXISTS "tbl_community_resources_categories" (
-    "community_resource_category_ulid" TEXT(26) PRIMARY KEY NOT NULL,
-    "display_text" TEXT NOT NULL,
-    "description" TEXT
-);
-CREATE UNIQUE INDEX "community_resources_categories_display_text_IDX" ON "tbl_community_resources_categories" ("display_text");
-CREATE TABLE IF NOT EXISTS "tbl_community_resources_categories_relations" (
-    "community_resource_relation_id" TEXT(26) PRIMARY KEY NOT NULL,
-    "community_resource_ulid_ref" TEXT(26) NOT NULL, -- Community resources
-    "community_resource_category_ulid_ref" TEXT(26), -- can have many categories
-    "game_ulid_ref" TEXT(26), -- for many games
-    FOREIGN KEY ("community_resource_category_ulid_ref") REFERENCES "tbl_community_resources_categories" ("community_resource_category_ulid"),
-    FOREIGN KEY ("community_resource_ulid_ref") REFERENCES "tbl_community_resources" ("community_resource_ulid")
-    FOREIGN KEY ("game_ulid_ref") REFERENCES "tbl_games" ("game_ulid")
-);
-CREATE UNIQUE INDEX "com_res_cat_ulids_IDX" ON "tbl_community_resources_categories_relations" ("community_resource_category_ulid_ref", "community_resource_ulid_ref", "game_ulid_ref");
-CREATE TABLE IF NOT EXISTS "tbl_profiles_statistics" (
-	"profile_statistic_ulid" TEXT(26) PRIMARY KEY NOT NULL,
-    -- "game_ulid_ref" TEXT(26), -- REL
-    -- "leaderboards_ulid_ref" TEXT(26), -- REL
-	"timestamp_dt" DATETIME NOT NULL,
-    "interval_days" INTEGER
-    -- TODO: Talk to Coolio on Discord
-    -- IDEAS:
-    -- - how many games per week
-    -- - how many losses/wins
-    -- - more chatty?
-    -- - which games played most? how long?
-    -- - most played civs (favourite civs)
-    -- - most wins with which civs
-    -- - most played map
-    -- - most won map (which civ?)
-    -- - how much time spent in queue
-    -- - percentage of 1v1 vs TG (both ranked)
-    -- - tends to pick civ (metric?)
-    -- - "Player X has a YY.Z% winrate vs <civ_x> in <year>/<month> on <map>"
-    -- - etc.
-	-- CONSTRAINT "statistics_game_ulid_ref_fkey" FOREIGN KEY ("game_ulid_ref") REFERENCES "games" ("ulid"),
-	-- CONSTRAINT "statistics_leaderboards_ulid_ref_fkey" FOREIGN KEY ("leaderboards_ulid_ref") REFERENCES "leaderboards" ("ulid")
-);
 CREATE TABLE IF NOT EXISTS "tbl_localizations" (
     "ulid" TEXT(26) PRIMARY KEY NOT NULL,
     "lang" TEXT (5) NOT NULL,
@@ -357,13 +247,6 @@ CREATE TABLE IF NOT EXISTS "tbl_localizations" (
 );
 CREATE UNIQUE INDEX "localizations_lang_IDX" ON "tbl_localizations" ("lang");
 CREATE INDEX "localizations_updated_at_dt_IDX" ON "tbl_localizations" ("updated_at_dt");
-CREATE TABLE IF NOT EXISTS "tbl_profiles_statistics_relations" (
-	"profile_statistic_relation_ulid" TEXT(26) PRIMARY KEY NOT NULL,
-    "profile_ulid_ref" TEXT(26) NOT NULL,
-    "profile_statistic_ulid_ref" TEXT(26) NOT NULL,
-	CONSTRAINT "profiles_statistics_relations_profile_ulid_ref_fkey" FOREIGN KEY ("profile_ulid_ref") REFERENCES "tbl_profiles" ("profile_ulid"),
-	CONSTRAINT "profiles_statistics_relations_profile_statistic_ulid_ref_fkey" FOREIGN KEY ("profile_statistic_ulid_ref") REFERENCES "tbl_profiles_statistics" ("profile_statistic_ulid")
-);
 -- Dbmate schema migrations
 INSERT INTO "db_schema_migrations" (version) VALUES
   ('20221019185730'),
@@ -371,8 +254,6 @@ INSERT INTO "db_schema_migrations" (version) VALUES
   ('20221019194233'),
   ('20221019203209'),
   ('20221019210509'),
-  ('20221019213552'),
-  ('20221019214838'),
   ('20221019215753'),
   ('20221019221428'),
   ('20221019222532'),
@@ -385,12 +266,5 @@ INSERT INTO "db_schema_migrations" (version) VALUES
   ('20221020151117'),
   ('20221020151258'),
   ('20221020152846'),
-  ('20221020152930'),
-  ('20221020153039'),
-  ('20221020172045'),
-  ('20221020180409'),
-  ('20221020233715'),
-  ('20221021000721'),
   ('20221021012325'),
-  ('20221025180211'),
-  ('20221030034120');
+  ('20221025180211');
