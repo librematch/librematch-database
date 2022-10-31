48cc0999f94546d383e79ad937a74c3ceff710a5
simonsan <14062932+simonsan@users.noreply.github.com>
2022-10-31 09:36:00 +0100

    Add information to users table

--- db/migrations/20221019193706_create_users_table.sql
+++ db/migrations/20221019193706_create_users_table.sql
@@ -2,8 +2,11 @@
 CREATE TABLE "tbl_users" (
 	"user_ulid" TEXT(26) PRIMARY KEY NOT NULL,
 	"profile_ulid_ref" TEXT(26) NULL UNIQUE,
-	"name" TEXT(50) NOT NULL,
-	"description" TEXT(255) NULL,
+	"user_name" TEXT(50) NOT NULL,
+	"email_address" TEXT(100) NULL,
+	"discord_id" TEXT(100) NULL,
+	"steam_name" TEXT(100) NULL,
+	"about_me" TEXT(255) NULL,
 	"rate_limit_per_unit" INTEGER DEFAULT 3,
 	"rate_limit_unit" INTEGER DEFAULT 0, -- 0=minute, 1=hour, 2=day, 3=month
 	"rate_limit_active" INTEGER DEFAULT 1 NOT NULL,
